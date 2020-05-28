af4ead1b47d5eeebd6cda6c86473cfbe5074d20b
chenmc <ccurricane@gmail.com>
2020-05-28 13:58:06 +0800

    学习c++语法中的模板,完成一个demo

---
+++ C++/grammar/Template/simpleDemo.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+
+template <typename T>
+class A
+{
+public:
+    A(T t)
+    {
+        mA = t;
+    }
+
+    void print()
+    {
+        std::cout << "mA :" << mA << std::endl;
+    }
+private:
+    T mA;
+};
+
+
+// 模板类派生普通的类
+// 派生时，父类需要指定类型
+class B:public A<int>
+{
+private:
+    int mB;
+public:
+    B(int b, int a);
+    ~B();
+};
+
+// 因为父类A只有有参构造函数，因此需要在构造列表中初始化
+B::B(int b, int a):A<int>(a)
+{
+    mB = b;
+}
+
+B::~B()
+{
+}
+
+
+// 模板类派生模模板类
+// 需要声明派生的类型 T
+//派生时，需要指定父类派生的类型T
+template <typename T>
+class C:public A<T> 
+{
+private:
+    T mC;
+public:
+
+    //因为父类A只有有参构造函数，因此需要在构造列表中初始化
+    // 派生类的函数，只能写在类里
+    C(T c, T a):A<T>(a)
+    {
+        mC = c;
+    }
+
+    void print();
+};
+
+template <typename T>
+void C<T>::print()
+{
+    std::cout << "mC: " << mC << std::endl;
+}
+
+// template <typename T>
+// C::C(T c, T a):A<T>(a)
+// {
+//     t = c;
+// }
+
+int main()
+{
+    A<int> a(10);
+    B b(11, 9);
+    C<int> c(12, 8);
+    a.print();
+    b.print();
+    c.print();
+}
