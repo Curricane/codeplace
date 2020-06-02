3e6ae660b8ed8bf2213cc9ad5a241e8c685160ed
chenmc <ccurricane@gmail.com>
2020-06-02 11:32:44 +0800

    增加了标准答案,用数组长度简化的代码

--- C++/sword2offer/033-SquenceOfBST/isSquenceOfBST.cpp
+++ C++/sword2offer/033-SquenceOfBST/isSquenceOfBST.cpp
@@ -117,6 +117,45 @@ bool checkLeft(int *arr, int root, int left, int right)
 }
 
 
+//  官方实现
+// BST：Binary Search Tree，二叉搜索树
+bool VerifySquenceOfBST(int sequence[], int length)
+{
+    if(sequence == nullptr || length <= 0)
+        return false;
+
+    int root = sequence[length - 1]; // 使用长度避免了一些麻烦，如左子树的位置
+
+    // 在二叉搜索树中左子树的结点小于根结点
+    int i = 0; // 左子树的长度
+    for(; i < length - 1; ++ i)
+    {
+        if(sequence[i] > root)
+            break;
+    }
+
+    // 在二叉搜索树中右子树的结点大于根结点
+    int j = i; 
+    for(; j < length - 1; ++ j)
+    {
+        if(sequence[j] < root)
+            return false;
+    }
+
+    // 判断左子树是不是二叉搜索树
+    bool left = true;
+    if(i > 0) // 
+        left = VerifySquenceOfBST(sequence, i);
+
+    // 判断右子树是不是二叉搜索树
+    bool right = true;
+    if(i < length - 1)
+        right = VerifySquenceOfBST(sequence + i, length - i - 1);
+
+    return (left && right);
+}
+
+
 
 // ====================测试代码====================
 void Test(const char* testName, int sequence[], int length, bool expected)
@@ -124,7 +163,7 @@ void Test(const char* testName, int sequence[], int length, bool expected)
     if(testName != nullptr)
         printf("%s begins: ", testName);
 
-    if(isSequenceBST(sequence, length) == expected)
+    if(VerifySquenceOfBST(sequence, length) == expected)
         printf("passed.\n");
     else
         printf("failed.\n");
