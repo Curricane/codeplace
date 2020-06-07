3fd3d5080b87ad5c8fa9f219f15a0874cab81c96
chenmc <ccurricane@gmail.com>
2020-06-07 18:32:24 +0800

    增加erase例子

--- C++/grammar/Vector/demo1.cpp
+++ C++/grammar/Vector/demo1.cpp
@@ -68,11 +68,26 @@ void test2()
 
 void test3()
 {
-    
+    int iArray[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> vecIntA(iArray, iArray + 9);
+    vector<int>::iterator it1 =  vecIntA.begin() + 2;
+    cout << *it1 << endl; // 3
+    vector<int>::iterator it2 =  vecIntA.erase(vecIntA.begin() + 2);
+    cout << *it1 << endl; // 4
+    cout << *it2 << endl; // 4
+    cout << "vecIntA: ";
+    printVector(vecIntA); //vecIntA: 1 2 4 5 6 7 8 9 
+    cout << endl;
+
+    vecIntA.erase(it2, it2 + 2);
+    cout << "vecIntA: ";
+    printVector(vecIntA); // vecIntA: 1 2 6 7 8 9
+    cout << endl;
 }
 
 int main()
 {
     // test1();
-    test2();
+    // test2();
+    test3();
 }
