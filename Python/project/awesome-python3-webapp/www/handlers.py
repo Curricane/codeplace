b7829792eb04e6da080e05ba04f5417e1e21c9d1
chenmc <ccurricane@gmail.com>
2020-11-20 11:38:20 +0800

    写一个mvc用例,并测试

--- Python/project/awesome-python3-webapp/www/handlers.py
+++ Python/project/awesome-python3-webapp/www/handlers.py
@@ -2,3 +2,18 @@
 # -*- coding: utf-8 -*-
 
 ' url handlers '
+
+import re, time, json, logging, hashlib, base64, asyncio
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(process)d %(thread)d %(levelname)s [%(funcName)s]: %(message)s')
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users':users
+    }
